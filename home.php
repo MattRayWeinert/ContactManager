9d6b62c17fe2225ac6801778e8a691d6f1f0c9d2
MattRayWeinert <43066793+MattRayWeinert@users.noreply.github.com>
2019-05-31 14:46:32 -0400

    Add files via upload

--- home.php
+++ home.php
@@ -22,7 +22,7 @@ function addContact()
 
   var myJSON = JSON.stringify(json);
 
-  xhr.open("POST", "http://www.ucfconman.com/addContact.php", true);
+  xhr.open("POST", "http://www.ucfconman.com/API/addContact.php", true);
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.send(myJSON);
 
@@ -32,6 +32,16 @@ function addContact()
   document.getElementById('email').value='';
   location.reload();
 }
+//function deleteContact()
+
+//function searchContact()
+
+//function logout() [logs user out upon closing browser window]
+
+window.onunload = function ()
+{
+  <?php session_unset(); session_destroy();?>
+}
 </script>
 
  <title>Home</title>
@@ -40,7 +50,7 @@ function addContact()
     table
     {
     border-collapse: collapse;
-    width: 50%;
+    width: 70%;
     color: #030405;
     font-family: monospace;
     font-size: 25px;
@@ -64,13 +74,14 @@ function addContact()
 </style>
 </head>
 <body>
- <h2 class="header" style="color: #4CAF50">Contact Manager</h2>
+ <h1 class="header" style="color: #4CAF50">Contact Manager</h1>
  <table>
  <tr>
-  <th>First Name</th> 
-  <th>Last Name</th> 
+  <th>First Name</th>
+  <th>Last Name</th>
   <th>Phone Number</th>
   <th>Email</th>
+  <th></th>
  </tr>
  <?php
   $conn = mysqli_connect("localhost", "unit_conman", "qwerty101", "unit_database");
@@ -82,15 +93,21 @@ function addContact()
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
     // output data of each row
-   while($row = $result->fetch_assoc()) {
+    while($row = $result->fetch_assoc()) {
     if($id == $row["id"]) {
-        echo "<tr><td>" . $row["firstName"]. "</td><td>" . $row["lastName"] . "</td><td>"
-        . $row["phoneNumber"]. "</td><td>" . $row["email"]. "</td></tr>";
+        echo "<tr><td>" . $row["firstName"] . "</td><td>" . $row["lastName"] . "</td><td>"
+        . $row["phoneNumber"]. "</td><td>" . $row["email"]. "</td><td><a href=\"API\deleteContact.php?id=".$row['id']."&firstName=".$row['firstName']."&lastName=".$row['lastName']."\">Delete</a></td></tr>";
       }
 }
+
 echo "</table>";
 } else { echo "0 results"; }
 $conn->close();
+
+function getRequestInfo()
+{
+  return json_decode(file_get_contents('php://input'), true);
+}
 ?>
 </table>
 <br>
@@ -100,7 +117,6 @@ $conn->close();
     <input type="text" id="phoneNumber" placeholder="Phone Number">
     <input type="text" id="email" placeholder="Email">
     <button type="button" id="addContactButton" onclick="addContact();"> Add Contact </button>
-    <button type="button" id="addContactButton" onclick="deleteContact();"> Delete Contact </button>
 </div>
 </body>
 </html>
